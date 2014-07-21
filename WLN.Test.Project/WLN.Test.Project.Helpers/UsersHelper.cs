9369d5688b22601d95efadc0b4c963b095c1737c
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-21 16:17:38 +0300

    Added helper to work with users

---
+++ WLN.Test.Project/WLN.Test.Project.Helpers/UsersHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLN.Test.Project.Helpers
+{
+    public class UsersHelper
+    {
+        public static string GeneratePassword()
+        {
+            using (var prng = new RNGCryptoServiceProvider())
+            {
+                return GenerateToken(prng);
+            }
+        }
+
+        public static string GenerateToken(RandomNumberGenerator generator)
+        {
+            var tokenBytes = new byte[16];
+            generator.GetBytes(tokenBytes);
+            return tokenBytes.GetHashCode().ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static string GetHash(string value, string passwordSault)
+        {
+            return (value + passwordSault).GetHashCode().ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
