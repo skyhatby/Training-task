6974fb8923e5272cf2ef94d80bee5cc040089489
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-21 15:38:29 +0300

    Added base entities for future Auth

---
+++ WLN.Test.Project/WLN.Test.Project.Model/AuthModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLN.Test.Project.Model.AuthModels
+{
+    class User : BaseClass
+    {
+        public virtual string Password { get; set; }
+        public virtual string PasswordSalt { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
