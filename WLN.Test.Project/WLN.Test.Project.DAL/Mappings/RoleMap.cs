e11b133bba2c7c684cc478c6fa0db61258e12cf5
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-22 15:31:27 +0300

    Added NHibernate reference and Mappings

---
+++ WLN.Test.Project/WLN.Test.Project.DAL/Mappings/RoleMap.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+using WLN.Test.Project.Model.AuthModels;
+
+namespace WLN.Test.Project.DAL.Mappings
+{
+    public class RoleMap : ClassMap<Role>
+    {
+        public RoleMap()
+        {
+            Map(x => x.Id);
+            Map(x => x.Name);
+        }
+    }
+}
---
+++ WLN.Test.Project/WLN.Test.Project.DAL/Mappings/UserMap.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using WLN.Test.Project.Model.AuthModels;
+
+namespace WLN.Test.Project.DAL.Mappings
+{
+    public class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Map(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.Password);
+            Map(x => x.PasswordSault);
+            References(x => x.Role);
+        }
+    }
+}
