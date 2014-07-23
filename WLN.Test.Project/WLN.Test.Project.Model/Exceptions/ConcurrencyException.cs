6345b024a3330c10e0265adfc5f01c1534a9e01a
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-23 15:17:56 +0300

    Added some Exceptions

---
+++ WLN.Test.Project/WLN.Test.Project.Model/Exceptions/ConcurrencyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WLN.Test.Project.Model.Exceptions
+{
+    [Serializable]
+    public class ConcurrencyException : RepositoryException
+    {
+        public string EntityName { get; private set; }
+        public object Identifier { get; private set; }
+
+        public ConcurrencyException(string message, string entityName, object identifier)
+            : base(message)
+        {
+            EntityName = entityName;
+            Identifier = identifier;
+        }
+    }
+}
---
+++ WLN.Test.Project/WLN.Test.Project.Model/Exceptions/RepositoryException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WLN.Test.Project.Model.Exceptions
+{
+    [Serializable]
+    public class RepositoryException : Exception
+    {
+        public RepositoryException(string message)
+            : base(message)
+        {
+        }
+
+        public RepositoryException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
