d234da3909b9b23ebb69e95890df69c8f5eeb9e6
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-23 15:24:41 +0300

    Some mistakes

--- WLN.Test.Project/WLN.Db/Tables/Membership/User.sql
+++ WLN.Test.Project/WLN.Db/Tables/Membership/User.sql
@@ -2,7 +2,7 @@
 (
 	[Id] BIGINT NOT NULL  IDENTITY,
     [Password] NVARCHAR(128) NOT NULL, 
-    [PasswordSault] NVARCHAR(128) NOT NULL,
+    [PasswordSalt] NVARCHAR(128) NOT NULL,
 
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
