c6cd05117f97c37ad679539f1c8e5c7e44b0da79
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-23 17:22:46 +0300

    Added ConnectionException

---
+++ WLN.Test.Project/WLN.Test.Project.Model/Exceptions/ConnectionException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WLN.Test.Project.Model.Exceptions
+{
+    [Serializable]
+    public class ConnectionException : RepositoryException
+    {
+        public ConnectionException(string message)
+            : base(message)
+        {
+        }
+
+        public ConnectionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
