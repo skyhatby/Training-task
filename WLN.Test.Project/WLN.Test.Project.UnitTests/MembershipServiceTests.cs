73d66fca05962b2f14d51afafcd88659af589b23
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-24 17:46:33 +0300

    Added Test project to solution

---
+++ WLN.Test.Project/WLN.Test.Project.UnitTests/MembershipServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WLN.Test.Project.Logic.Membership.Intarfaces;
+
+namespace WLN.Test.Project.UnitTests
+{
+    [TestClass]
+    public class MembershipServiceTests
+    {
+        private IMembershipService _membershipService;
+
+        public MembershipServiceTests(IMembershipService membershipService)
+        {
+            _membershipService = membershipService;
+        }
+
+        [TestMethod]
+        public void GetRole()
+        {
+            var role = _membershipService.GetRoleByName("Administrator");
+            Assert.IsNotNull(role);
+
+            role.Dump();
+        }
+    }
+}
