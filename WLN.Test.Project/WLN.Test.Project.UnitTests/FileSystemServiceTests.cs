c50ebfadfc9c3a22d398ab2b4ba21357247a344e
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-28 13:08:16 +0300

    Added tests for FileSystemService

---
+++ WLN.Test.Project/WLN.Test.Project.UnitTests/FileSystemServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WLN.Test.Project.Logic.FileSystem.Interfaces;
+
+namespace WLN.Test.Project.UnitTests
+{
+    [TestClass]
+    public class FileSystemServiceTests : BaseTest
+    {
+        private IFileSystemService _fileSystemService;
+
+        public FileSystemServiceTests()
+        {
+            _fileSystemService = DependencyResolver.Resolve<IFileSystemService>();
+        }
+
+        [TestMethod]
+        public void GetDirectories()
+        {
+            var c = _fileSystemService.GetAllFoldersFromDirectory("C:\\");
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void GetFilesFromDirectory()
+        {
+            var c = _fileSystemService.GetAllFilesFromDirectory("C:\\");
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void GetFileByPath()
+        {
+            var c = _fileSystemService.GetFileByPath("C:\\Users\\desktop.ini");
+            Assert.IsNotNull(c);
+        }
+    }
+}
