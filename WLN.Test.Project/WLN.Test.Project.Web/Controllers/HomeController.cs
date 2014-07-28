b2fbf57be3c8cde844547f240539c960473cf1a3
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-28 16:01:31 +0300

    Added functionality to View folders and files

--- WLN.Test.Project/WLN.Test.Project.Web/Controllers/HomeController.cs
+++ WLN.Test.Project/WLN.Test.Project.Web/Controllers/HomeController.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WLN.Test.Project.Logic.FileSystem.Interfaces;
 
 namespace WLN.Test.Project.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IFileSystemService _fileSystemService;
+
+        public HomeController(IFileSystemService fileSystemService)
         {
-            return View();
+            _fileSystemService = fileSystemService;
+        }
+
+        public ActionResult Index(string path = "")
+        {
+            var drives = _fileSystemService.GetDrives();
+            ViewBag.CurrentDirectory = path;
+            return View(drives);
+        }
+
+        public PartialViewResult GetListOfDirectoriesPartial(string path)
+        {
+            if (!String.IsNullOrEmpty(path)) 
+            { 
+            var model = _fileSystemService.GetAllFoldersFromDirectory(path);
+            var parentFolderAddress = _fileSystemService.GetDirectoryByPath(path).Parent;
+            ViewBag.Parent = parentFolderAddress != null ? parentFolderAddress.FullName : "";
+            return PartialView(model);
+            }
+            return null;
+        }
+
+        public PartialViewResult GetListOfFilesPartial(string path)
+        {
+            if (!String.IsNullOrEmpty(path))
+            {
+                return PartialView(_fileSystemService.GetAllFilesFromDirectory(path));
+            }
+            return null;
         }
 
         public ActionResult About()
