d6db1cc8c1bdceb1944a1d549f681fd61be558e9
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-29 11:39:48 +0300

    Added Identity models

---
+++ WLN.Test.Project/WLN.Test.Project.Logic/Membership/Identity/UserIdIdentity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLN.Test.Project.Logic.Membership.Identity
+{
+    public class UserIdIdentity : IIdentity
+    {
+        public string Name { get; set; }
+        public long UserId { get; set; }
+        public string AuthenticationType { get { return "UserIdIdentity"; } }
+        public bool IsAuthenticated { get; set; }
+    }
+}
---
+++ WLN.Test.Project/WLN.Test.Project.Logic/Membership/Identity/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLN.Test.Project.Logic.Membership.Identity
+{
+    public class UserInfo
+    {
+        public long UserId { get; set; }
+
+        public override string ToString()
+        {
+            return UserId.ToString();
+        }
+
+        public static UserInfo FromString(string s)
+        {
+            return new UserInfo { UserId = Int64.Parse(s) };
+        }
+    }
+}
