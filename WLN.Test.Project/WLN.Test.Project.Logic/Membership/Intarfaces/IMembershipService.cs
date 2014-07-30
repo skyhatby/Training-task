ca9bea49d0114b4b7554e3b7048c0186bfd727dd
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2014-07-30 14:01:24 +0300

    Added XMLDoc comments to Interface of MembershipService

--- WLN.Test.Project/WLN.Test.Project.Logic/Membership/Intarfaces/IMembershipService.cs
+++ WLN.Test.Project/WLN.Test.Project.Logic/Membership/Intarfaces/IMembershipService.cs
@@ -10,12 +10,27 @@ namespace WLN.Test.Project.Logic.Membership.Intarfaces
 {
     public interface IMembershipService : IService
     {
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         Role GetRoleByName(string roleName);
+
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         User GetUser(long userId);
+
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         User GetUserByName(string userName);
+
+        /// <exception cref="WLN.Test.Project.Logic.Membership.MembershipServiceException"></exception>
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         User RegisterUser(string name, string password, string roleName = "User");
+
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         void UpdateUser(User user);
+
+        /// <exception cref="WLN.Test.Project.Logic.Membership.MembershipServiceException"></exception>
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         void ResetPassword(string name);
+
+        /// <exception cref="WLN.Test.Project.Logic.Common.ServiceException"></exception>
         string[] GetUserRoles(long userId);
     }
 }
